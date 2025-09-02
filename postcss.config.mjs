bc4ebf1a3baa6f746149ebf74adc0f3968d48f69
Goodness Nwanebu <goodnessnwanebu@gmail.com>
2025-09-02 11:18:41 +0100

    stable version

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,10 +2,10 @@ import postcssColorFunctionalNotation from 'postcss-color-functional-notation';
 import postcssOKLabFunction from '@csstools/postcss-oklab-function';
 
 export default {
-  plugins: [
-    postcssColorFunctionalNotation(),
-    postcssOKLabFunction(),
-    '@tailwindcss/postcss',
-    'autoprefixer',
-  ],
+  plugins: {
+    'postcss-color-functional-notation': {},
+    '@csstools/postcss-oklab-function': {},
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
+  },
 };
